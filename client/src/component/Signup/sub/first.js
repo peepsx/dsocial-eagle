6c0b26fbaf4c441f6dddb9c4d28c95c8373513b6
gaurav563 <563gauravshakya@gmail.com>
2020-02-07 11:30:56 +0530

    front end updates

--- client/src/component/Signup/sub/first.js
+++ client/src/component/Signup/sub/first.js
@@ -2,17 +2,34 @@ import React from 'react';
 import Axios from 'axios';
 import TwitterLogin from "react-twitter-login";
 import Button from './twitterButton';
+// import TelegramLoginButton from 'react-telegram-login';
 
 export default class First extends React.Component {
     constructor(props) {
         super(props);
-        this.handleFbClick = this.handleFbClick.bind(this);
+        this.handleTelegramResponse = this.handleTelegramResponse.bind(this);
     }
 
-    handleFbClick() {
+    componentDidMount() {
+        window.TelegramLoginWidget = {
+            dataOnauth: user => this.handleTelegramResponse(user)
+        };
+        const script = document.createElement('script');
+        script.src = 'https://telegram.org/js/telegram-widget.js?4';
+        script.setAttribute('data-telegram-login', 'ArisenIO_bot');
+        script.setAttribute('data-size', 'large');
+        script.setAttribute('data-request-access', "write");
+        script.setAttribute('data-userpic', false);
+        script.setAttribute('data-onauth', 'TelegramLoginWidget.dataOnauth(user)');
+        script.async = true;
+        this.instance.appendChild(script);
+    }
+
+    handleFbClick = () => {
         if (window.FB) {
             window.FB.login(function (response) {
                 if (response.status === 'connected') {
+                    console.log('response', response)
                     const userId = response.authResponse.userID.replace(/"/, ""), userAccessToken = response.authResponse.accessToken.replace(/"/, "");
                     Axios({
                         method: 'GET',
@@ -28,28 +45,39 @@ export default class First extends React.Component {
                     alert('User Login failed')
                 }
             }, {
-                scope: 'email',
+                scope: 'email,user_photos',
                 return_scoper: true,
             });
         }
     }
 
     handleInstaClick = () => {
-        window.open('https://api.instagram.com/oauth/authorize?client_id=185483479189128&redirect_uri=https://www.devgenesis.com/&scope=user_profile&response_type=code','_blank')
+        // window.open('https://api.instagram.com/oauth/authorize?client_id=185483479189128&redirect_uri=https://www.devgenesis.com/&scope=user_profile&response_type=code','_blank')
+        const code = 'AQA1rLxFKkXHu4LPvlNjAOx1ILno-lx_4YQ8XncIpxYnbjxKtfhdMUjeVQ8_VDnhZFYI19RD7_qCsUBk9V4Rn-lBC4qu9GWlTfFng6JrEikO_oKvNjR_wiXrl9m8IvrmmWJTzfKYbQ7hJTXBIbbc3hV3FQDjr4c_tzH1p79FjlQLpDFxBKb4tPyIW4pFeu0IfNrxhIZgTG9hX9I8gpFEvoISQDtlHJDBtyRPEyGuRtzGmg'
+        const data = new FormData()
+        data.append('client_id', process.env.instagram_client_id);
+        data.append('client_secret', process.env.instagram_client_secret_id);
+        data.append('grant_type', 'authorization_code');
+        data.append('redirect_uri', 'https://www.devgenesis.com/');
+        data.append('code', code);
 
+        fetch('https://api.instagram.com/oauth/access_token', {
+            method: 'POST',
+            body: data
+        })
+            .then(res => res.json())
+            .then(response => console.log('response', response))
+            .catch(err => console.log('error', err))
     }
 
-    handleGoogleClick(googleUser) {
+    handleGoogleClick = () => {
         window.gapi.load('auth2', function () {
             /* Ready. Make a call to gapi.auth2.init or some other API */
             window.gapi.auth2.init({
-                client_id: '1017408504940-nti1ddvjutoittciqkoavk442taafj9m.apps.googleusercontent.com',
+                client_id: process.env.google_client_id + '.apps.googleusercontent.com',
                 cookiepolicy: 'single_host_origin',
-                // Request scopes in addition to 'profile' and 'email'
-                //scope: 'additional_scope'
             }).then(() => {
                 const auth2 = window.gapi.auth2.getAuthInstance();
-                // auth2.isSignedIn.listen(res);
                 auth2.signIn().then(res => {
                     console.log('after sign in response', res)
                 })
@@ -58,22 +86,16 @@ export default class First extends React.Component {
 
     }
 
-    handleTele = (user) => {
-        console.log(user)
-        // window.open('https://telegram.org/js/telegram-widget.js?7&data-telegram-login="freedom563bot"&data-size="large"&data-auth-url="http://localhost:3000/','_blank')
-    }
-
-    randomString = function (length) {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-    }
+    handleTelegramResponse(response){
+        console.log(response);
+    };
 
     authHandler = (err, authData) => {
         console.log('response', err, authData)
+    }
+
+    componentWillReceiveProps(props){
+        console.log('Props Recieved:', props)
     }
 
     render() {
@@ -96,7 +118,7 @@ export default class First extends React.Component {
                             consumerKey={process.env.twitter_consumer_key}
                             consumerSecret={process.env.twitter_consumer_secret_key}
                             callbackUrl={"http://localhost:3000/"}
-                            children={<Button/>}
+                            children={<Button />}
                         />
                     </div>
                     <div className="col-sm mb-3 mb-sm-0">
@@ -111,13 +133,20 @@ export default class First extends React.Component {
                             <span className="h6 mb-0 d-block">Google</span>
                         </button>
                     </div>
-                    <div className="col-sm mb-3 mb-sm-0">
-                        <a onClick={this.handleTele} className="btn btn-block btn-outline-light border py-4 h-100">
-                            <img className="icon mb-3" src="assets/img/arisen/telegram.png" alt="google" />
-                            <span className="h6 mb-0 d-block">Telegram Community</span>
-                        </a>
-                    </div>
                 </div>
+                <a className="btn btn-outline-light border">
+                    {/* <img className="icon mb-3" src="assets/img/arisen/telegram.png" alt="google" />
+                        <span className="h6 mb-0 d-block">Telegram Community</span> */}
+                    <div
+                        className={this.props.className}
+                        ref={component => {
+                            this.instance = component;
+                        }}
+                    >
+                        {this.props.children}
+                    </div>
+                    {/* <TelegramLoginButton dataOnauth={this.handleTelegramResponse} botName="ArisenIO_bot" /> */}
+                </a>
                 <div className="d-flex justify-content-center pb-0 pt-3">
                     <button className="btn btn-primary sw-btn-next">Next Step</button>
                 </div>
--- client/src/component/Signup/sub/second.js
+++ client/src/component/Signup/sub/second.js
@@ -3,38 +3,58 @@ import React from 'react'
 export default class Second extends React.Component {
     render() {
         return (
-<div className="card-body py-4">
+            <div className="card-body py-4">
                 <div className="mb-4 text-center">
-                    {/* <span className="h3 d-block">What kind of business are you?</span> */}
                     <p className="lead">Please follow Arisen with logged in accounts.</p>
                 </div>
                 <div className="row">
                     <div className="col-sm mb-3 mb-sm-0">
-                        <button className="btn btn-block btn-outline-light border py-4 hover-white" type="button">
+                        <button className="btn btn-block btn-outline-light border py-4 h-100 hover-white" type="button">
                             <img className="icon mb-3" src="assets/img/arisen/facebook.png" alt="facebook" />
                             <span className="h6 mb-0 d-block">Facebook Page</span>
-                            <a className="btn btn-sm btn-primary mt-2 hover-white" type="button">Like</a>
+                            <div
+                                className="fb-like mt-2"
+                                data-href="https://www.facebook.com/3DModels.48/"
+                                data-width=""
+                                data-layout="button"
+                                data-action="like"
+                                data-size="large"
+                            >
+                            </div>
                         </button>
                     </div>
                     <div className="col-sm mb-3 mb-sm-0">
-                        <button className="btn btn-block btn-outline-light border py-4 hover-white" type="button">
+                        <button className="btn btn-block btn-outline-light border py-4 h-100 hover-white" type="button">
                             <img className="icon mb-3" src="assets/img/arisen/twitter.png" alt="twitter" />
                             <span className="h6 mb-0 d-block">Twitter Handle</span>
-                            <a className="btn btn-sm btn-info mt-2 hover-white" type="button">Follow</a>
+                            <div className="mt-2">
+                                <a href="https://twitter.com/Ashutos83977501?ref_src=twsrc%5Etfw" className="twitter-follow-button"
+                                    data-show-screen-name="false"
+                                    data-show-count="false"
+                                    data-size="large"
+                                ></a>
+                            </div>
                         </button>
                     </div>
                     <div className="col-sm mb-3 mb-sm-0">
-                        <button className="btn btn-block btn-outline-light border py-4 hover-white" type="button">
+                        <button className="btn btn-block btn-outline-light border py-4 h-100 hover-white" type="button">
                             <img className="icon mb-3" src="assets/img/arisen/instagram.png" alt="instagram" />
                             <span className="h6 mb-0 d-block">Instagram Page</span>
                             <a className="btn btn-sm btn-danger mt-2 hover-white" type="button">Follow</a>
                         </button>
                     </div>
                     <div className="col-sm mb-3 mb-sm-0">
-                        <button className="btn btn-block btn-outline-light border py-4 hover-white" type="button">
+                        <button className="btn btn-block btn-outline-light border py-4 h-100 hover-white" type="button">
                             <img className="icon mb-3" src="assets/img/arisen/youtube.png" alt="google" />
                             <span className="h6 mb-0 d-block">Youtube Channel</span>
-                            <a className="btn btn-sm mt-2 btn-red hover-white" type="button">Subscribe</a>
+                            <div className="mt-2">
+                                <div 
+                                    className="g-ytsubscribe" 
+                                    data-channelid="UC2foi1ia54oj0TxlkDYXV9g" 
+                                    data-layout="default" 
+                                    data-count="hidden">
+                                </div>
+                            </div>
                         </button>
                     </div>
                 </div>
