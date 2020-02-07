d49e2b7896a46a83de216a8aa0f3b396ced6bbd3
sbsashu <sbsashu143@gmail.com>
2020-02-07 16:58:02 +0530

    test_api

--- server/routes/test_api.js
+++ server/routes/test_api.js
@@ -2,7 +2,7 @@ let express  = require('express');
 
 let router = express.Router();
 
-router.get('/test-api', async (req, res) => {
+router.get('test-api/', async (req, res) => {
     return res.status(200).send({
         message: 'Test-api running sucessfully '
     })
