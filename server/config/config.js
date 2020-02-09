8ada2e53599cd58d32512af5e47408a255bfcd2f
sbsashu <sbsashu143@gmail.com>
2020-02-09 14:22:41 +0530

    RSN_TRANSFER_API

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
     URL: process.env.DB_URI || "mongodb://localhost:27017/airdrop-token-exchange",
     PORT: process.env.PORT || 8001
---
+++ server/routes/rsn_transfer.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let config = require('../config/arisen');
+let rsn = require('arisenjsv1');
+
+let router = express.Router();
+
+router.post('/rsn-transfer', (req, res) => {
+        let { } = req.body;
+})
+
+module.exports = router;
