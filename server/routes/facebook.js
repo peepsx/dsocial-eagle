ae093a302b114e33bf171869008bff66b3e1dabd
sbsashu <sbsashu143@gmail.com>
2020-02-10 13:11:27 +0530

    model_name_reolve

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const FB = require('../models/facebook')
+const {faceAuth} = require('../models/facebook')
 
 router.post('/facebook_detail',async (req,res,next)=>{
     console.log(req.body)
     let { fbUserURL, fbPhoto,fbUserName, fbUserLocation} = req.body;
-    let findOne = await FB.findOne({fbUserName: fbUserName})
+    let findOne = await faceAuth.findOne({fbUserName: fbUserName})
 
      if(fbUserURL && fbPhoto && fbUserName && fbUserLocation && !findOne ==null ) {
 
-            let newFbUser = new FB({
+            let newFbUser = new faceAuth({
                 fbUserURL: fbUserURL,
                 fbPhoto: fbPhoto,
                 fbUserName: fbUserName,
