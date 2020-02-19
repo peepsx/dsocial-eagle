945617a060a333512fb47790cb588319ca9e1d25
gaurav563 <563gauravshakya@gmail.com>
2020-02-19 19:01:20 +0530

    fancount done

--- client/src/component/js/fbPageApi.js
+++ client/src/component/js/fbPageApi.js
@@ -1,10 +1,8 @@
-import Axios from "axios"
+import Axios from "axios";
 
 export const getFbPageToken = (id, userAccessToken) => {
-    console.log('access token and id insife page token', id, userAccessToken);
     Axios.get(`https://graph.facebook.com/${id}/accounts?access_token=${userAccessToken}`)
         .then(res => {
-            console.log('Page access toke and id ', res);
             if (res.data && res.data.data) {
                 getFbPageCount(res.data.data[0].id, res.data.data[0].access_token)
             }
@@ -13,7 +11,6 @@ export const getFbPageToken = (id, userAccessToken) => {
 }
 
 export const getFbPageCount = (id, pageAccessToken) => {
-    console.log('access token and id insife count', id, pageAccessToken);
     Axios.get(`https://graph.facebook.com/${id}/?fields=fan_count&access_token=${pageAccessToken}`)
         .then(res => console.log('page count', res))
         .catch(err => console.error('page count error', err))
