dfcbe2b9add340b62f2eec15468947bf50b17333
gaurav563 <563gauravshakya@gmail.com>
2020-02-20 16:55:57 +0530

    test google

--- client/src/component/Signup/forms/login_buttons/google.js
+++ client/src/component/Signup/forms/login_buttons/google.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Axios from 'axios';
 
-import {API} from '../../../js/api_list';
+import { API } from '../../../js/api_list';
 import { env } from '../../../config/config';
 import { toast } from 'react-toastify';
 
 export default class Google extends React.Component {
 
     handleGoogleClick = () => {
-        window.gapi.load('auth2',() => {
+        window.gapi.load('auth2', () => {
             /* Ready. Make a call to gapi.auth2.init or some other API */
             window.gapi.auth2.init({
                 client_id: env.google_client_id,
@@ -16,25 +16,20 @@ export default class Google extends React.Component {
             }).then(() => {
                 const auth2 = window.gapi.auth2.getAuthInstance();
                 auth2.signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" }).then(res => {
-                    console.log('response',res)
-                    for( let i of res) {
-                        if(i && i.access_token){
-                            console.log('called',i.access_token)
-                            this.handleGoogleDataSave(res,i.access_token);
-                        }
-                    }
                     window.gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest");
+                    window.google = res;
+                    this.handleGoogleDataSave(res);
                 })
             })
         });
     }
 
-    handleGoogleDataSave = (userData,access_token) => {
+    handleGoogleDataSave = (userData) => {
         if (userData) {
             const data = JSON.stringify(userData);
             const email = data.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-            console.log('email google',email)
-            localStorage.setItem('googleEmail',email);
+            console.log('email google', email)
+            localStorage.setItem('googleEmail', email);
             Axios({
                 url: API.google_detai,
                 method: 'POST',
@@ -65,11 +60,11 @@ export default class Google extends React.Component {
 
     render() {
         return (
-            <button 
-                onClick={this.handleGoogleClick} 
-                className="btn btn-block btn-outline-light border py-4 h-100" 
+            <button
+                onClick={this.handleGoogleClick}
+                className="btn btn-block btn-outline-light border py-4 h-100"
                 type="button"
-                // disabled={!(this.props.nextBtnStatus === 'Google')}
+            // disabled={!(this.props.nextBtnStatus === 'Google')}
             >
                 <img className="icon mb-3" src="assets/img/arisen/google.png" alt="google" />
                 <span className="h6 mb-0 d-block">Google</span>
