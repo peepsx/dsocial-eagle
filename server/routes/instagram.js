130bd351f4e30001e3785d47ed3b7f3a427b54db
sbsashu <sbsashu143@gmail.com>
2020-03-04 12:39:15 +0530

    instagram change api

--- server/routes/instagram.js
+++ server/routes/instagram.js
@@ -9,14 +9,14 @@ const { IgApiClient } = require('instagram-private-api');
 
 router.post('/instagram-details', [Access_Token], async (req, res)=>{
         
-    let { username, password } = req.body;
-        if(!username && !password ) {
+    let { username } = req.body;
+        if(!username) {
             return res.status(400).send({
                 success: false,
                 message: "Fields are missing"
             })
         }
-        if(username == undefined || password == undefined || username == null || password == null ) {
+        if(username == undefined || username == null || typeof username === String) {
             return res.status(400).send({
                 success: false,
                 message: 'Fields are missing'
@@ -39,27 +39,18 @@ router.post('/instagram-details', [Access_Token], async (req, res)=>{
         }
 
         try {
-            const ig = new IgApiClient();
-            ig.state.generateDevice(username)
-            const auth = await ig.account.login(username, password);
-            const followersFeed = ig.feed.accountFollowers(auth.pk);
-            const login = await followersFeed.request();
-            // const client = new Instagram({username, password}); delete after final test
-            // let login = await client.login(); delete after final test
-            // if(!login.authenticated) return res.status(404).json({success: false, message: 'Invalid instagram id'});
-            // const followers = await client.getFollowers({ userId: login.userId }) delete after final test
+            let instagram = await axios.get(`https://www.instagram.com/${username}/?__a=1`)
             
-            if(login) {
-                let newInsta = new TempInstagram({
-                    instaid: auth.pk,
-                    follower: login.users.length,
-                    username: auth.username
+            let newInsta = new TempInstagram({
+                    instaid: instagram.data.graphql.user.id,
+                    follower: instagram.data.graphql.user.edge_followed_by.count,
+                    username: instagram.data.graphql.user.username
                 })
                 newInsta.save()
                     .then(insta => {
                         res.status(200).send({
                             success: true,
-                            data:{name: username, pass: password},
+                            data:{name: username},
                             message: 'You have logged in successfully!'
                         })
 
@@ -70,28 +61,10 @@ router.post('/instagram-details', [Access_Token], async (req, res)=>{
                             message: 'Something went wrong while saving User Data'
                         })
                     })
-            } else {
-                return res.status(401).send({
-                    success:  false,
-                    message: 'Something went wrong'
-                })
-            }
 
         } catch(error) {
             console.log("INSTAGRAM ERROR", error.message);
 
-            if(error.message === "POST /api/v1/accounts/login/ - 400 Bad Request; The username you entered doesn't appear to belong to an account. Please check your username and try again.") {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Username is invalid'
-                })
-            }
-            if(error.message === "POST /api/v1/accounts/login/ - 400 Bad Request; The password you entered is incorrect. Please try again.") {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Password is invalid'
-                })
-            }
             if(error.message === 'Request failed with status code 404') return res.status(404).send({success: false, message: `Instagram user ${username} not found`});
 
             return res.status(500).send({
