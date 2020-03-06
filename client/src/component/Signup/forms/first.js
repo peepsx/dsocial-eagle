f1e1307188e8a9d1fd551bb6310da505a66705eb
gaurav563 <563gauravshakya@gmail.com>
2020-03-06 10:04:53 +0530

    step 1 validatiion change

--- client/src/component/Signup/forms/first.js
+++ client/src/component/Signup/forms/first.js
@@ -43,7 +43,6 @@ export default class First extends React.Component {
         const googleEmail = localStorage.getItem('googleEmail');
         const instaUserId = localStorage.getItem('instaUserId');
         const twitterName = localStorage.getItem('twitterName');
-        if (!localStorage.getItem('s1')) {
             if (!fbData || !googleEmail || !instaUserId || !twitterName) {
                 this.setState({ loading: false })
                 Swal.fire({
@@ -87,15 +86,6 @@ export default class First extends React.Component {
                     confirmButtonText: 'Okay',
                 })
             }
-        } else {
-            Swal.fire({
-                title: 'Error',
-                text: 'Invalid Step !!',
-                icon: "error",
-                showCancelButton: false,
-                confirmButtonText: 'Okay',
-            })
-        }
     }
 
     handleTwitDataSave = (userData) => {
