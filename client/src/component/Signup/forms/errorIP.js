5dc43c5b6383367740dee1a4d31feef4f0985629
gaurav563 <563gauravshakya@gmail.com>
2020-03-13 11:47:18 +0530

    after testing

--- client/src/component/Signup/forms/errorIP.js
+++ client/src/component/Signup/forms/errorIP.js
@@ -38,7 +38,7 @@ export default class Ipexist extends React.Component {
                                 <h4 className="modal-title text-center">Contact Us</h4>
                             </div>
                             <div className="modal-body py-2">
-                                <p className="mb-0">If you are facing any issues in completing the steps or have any kind of problem.<br /> Please contact us via email, send us a descriptive issue on <span className="h5 color-orange">dpeepsproject@gmail.com</span></p>
+                                <p className="mb-0">If you are facing any issues in completing the steps or have any kind of problem.<br /> Please contact us via email, send us a descriptive issue report on <span className="h5 color-orange">dpeepsproject@gmail.com</span></p>
                             </div>
                             <div className="modal-footer justify-content-center">
                                 <button type="button" className="btn btn-custom h-2 w-8" data-dismiss="modal">Okay</button>
