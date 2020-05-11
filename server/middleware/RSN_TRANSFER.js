0e779dafd0af25b9d1180916427c7c87d56828a1
sbsashu <sbsashu143@gmail.com>
2020-05-11 15:36:17 +0530

    change for instagram and telegram

--- server/middleware/RSN_TRANSFER.js
+++ server/middleware/RSN_TRANSFER.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 let { TempFacebook } = require('../models/TempFacebook')
 let { TempGoogle } = require('../models/TempGoogle')
-let { TempInstagram } = require('../models/TempInstagram')
+// let { TempInstagram } = require('../models/TempInstagram')
 let { TempTwitter } = require('../models/TempTwitter')
-let { TempTelegram } = require('../models/TempTelegram')
+// let { TempTelegram } = require('../models/TempTelegram')
 let jwt = require('jsonwebtoken');
 
 module.exports = {
     RSN_TRANSFER: async (req, res, next) => {
-        let { fbUserId, googleEmail, instaUserId, teleUserId, twitterScreenName } = req.body.userDetails;
-        if( !googleEmail || !instaUserId || !twitterScreenName || !teleUserId || !fbUserId) {
+        let { fbUserId, googleEmail, /**instaUserId, teleUserId, */ twitterScreenName } = req.body.userDetails;
+        if( !googleEmail || !twitterScreenName || /**!instaUserId ||  !teleUserId || */ !fbUserId) {
             return res.status(200).send({
                 success: false,
                 message: 'Fields are missing'
             })
         }
-        if(fbUserId == undefined  || googleEmail == undefined || instaUserId == undefined || twitterScreenName == undefined) {
+        if(fbUserId == undefined  || googleEmail == undefined || /**instaUserId == undefined || */ twitterScreenName == undefined) {
             return res.status(200).send({
                 message: 'Fields are missing'
             })
         }
         let facebook = await TempFacebook.findOne({facebookid: fbUserId});
         let google = await TempGoogle.findOne({GmailAddress: googleEmail});
-        let instagram = await TempInstagram.findOne({username: instaUserId});
+        // let instagram = await TempInstagram.findOne({username: instaUserId});
         let twitter = await TempTwitter.findOne({username: twitterScreenName});
-        let telegram = await TempTelegram.findOne({telegram_id: teleUserId});
+        // let telegram = await TempTelegram.findOne({telegram_id: teleUserId});
         if(!facebook.facebookid || !facebook.fbUserName || facebook === null ) {
             return res.status(401).send({
                 success: false,
@@ -35,22 +35,22 @@ module.exports = {
                 success: false,
                 message: 'Please Login with Google'
             })
-        } else if(!instagram.instaid || !instagram.username || instagram === null) {
+        } /**else if(!instagram.instaid || !instagram.username || instagram === null) {
             return res.status(401).send({
                 success: false,
                 message: 'Please Login with Instagram'
             })
-        } else if(!twitter.username || twitter === null) {
+        } */ else if(!twitter.username || twitter === null) {
             return res.status(401).send({
                 success: false,
                 message: 'Please Login with Twitter'
             })
-        } else if(!telegram.telegram_id || telegram === null) {
+        }/** else if(!telegram.telegram_id || telegram === null) {
             return res.status(401).send({
                 success: false,
                 message: 'Please Login with Twitter'
             })
-        }
+        } */
         else {
             next();
         }
--- server/routes/telegaramapi.js
+++ server/routes/telegaramapi.js
@@ -1,44 +1,44 @@
-'use strict';
+// 'use strict';
 
-let express = require('express');
-let router = express.Router();
-let { TelegramDetail } = require('../models/telegram')
-let { TempTelegram } = require('../models/TempTelegram')
-let { Access_Token } = require('../middleware/RSN_TRANSFER');
+// let express = require('express');
+// let router = express.Router();
+// let { TelegramDetail } = require('../models/telegram')
+// let { TempTelegram } = require('../models/TempTelegram')
+// let { Access_Token } = require('../middleware/RSN_TRANSFER');
 
-router.post('/telegram', [Access_Token], async (req, res) => {
-    let { id, first_name, last_name } = req.body;
+// router.post('/telegram', [Access_Token], async (req, res) => {
+//     let { id, first_name, last_name } = req.body;
     
-    if(!id || !first_name || !last_name) return res.status(400).send({success: false,message: 'Fields are missing'});
+//     if(!id || !first_name || !last_name) return res.status(400).send({success: false,message: 'Fields are missing'});
 
-    let TempTele = await TempTelegram.findOne({telegram_id: id});
-    if(TempTele)  return res.status(200).send({
-        success: true,
-        message:'You have logging successfully!'
-    });
+//     let TempTele = await TempTelegram.findOne({telegram_id: id});
+//     if(TempTele)  return res.status(200).send({
+//         success: true,
+//         message:'You have logging successfully!'
+//     });
     
-    let checkTelegram = await TelegramDetail.findOne({telegram_id: id});
+//     let checkTelegram = await TelegramDetail.findOne({telegram_id: id});
     
-    if(checkTelegram) return res.status(403).send({success: false, message: 'You have already register with us!'});
+//     if(checkTelegram) return res.status(403).send({success: false, message: 'You have already register with us!'});
     
-    try {
-        let newTelegram = new TempTelegram({
-            telegram_id: id,
-            username: first_name+" "+last_name
-        })
-         await newTelegram.save();
+//     try {
+//         let newTelegram = new TempTelegram({
+//             telegram_id: id,
+//             username: first_name+" "+last_name
+//         })
+//          await newTelegram.save();
         
-        return res.status(200).send({
-            success: true,
-            message: 'You have logged in successfully!'
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({
-            success: false,
-            message: "Server Error"
-        })
-    }
-})
+//         return res.status(200).send({
+//             success: true,
+//             message: 'You have logged in successfully!'
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send({
+//             success: false,
+//             message: "Server Error"
+//         })
+//     }
+// })
 
-module.exports = router;
+// module.exports = router;
