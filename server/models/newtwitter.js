4cb8cc594a1a4d69e596c44ae3a9f1b2d88541e1
sbsashu <sbsashu143@gmail.com>
2020-07-06 18:50:28 +0530

    modal changes

--- server/models/newtwitter.js
+++ server/models/newtwitter.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 
   var UserSchema = new Schema({
     email: {
-      type: String, required: true,
+      type: String
       trim: true, unique: true,
       match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     },
