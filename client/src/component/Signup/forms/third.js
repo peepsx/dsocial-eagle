2e2728deb29a94de70813cd76e3f937a5bc0211f
sbsashu <sbsashu143@gmail.com>
2020-07-21 16:51:20 +0530

    share with other user

--- client/src/component/Signup/forms/third.js
+++ client/src/component/Signup/forms/third.js
@@ -122,8 +122,8 @@ export default class Third extends React.Component {
     render() {
         return localStorage.getItem('username') || localStorage.getItem('twitterName') || localStorage.getItem('googleEmail') || localStorage.getItem('fbUserId') ? (
             <div className="card-body py-4">
-                <img src={gold} alt='gold' width="15 px" height="auto"></img> <span>0 RIX</span>
                 <div className="mb-4 text-center">
+                    <img src={gold} alt='gold' width="15 px" height="auto"></img> <span>0 RIX</span>
                     <span className="h4 d-block">Spread the word about dSocial...</span>
                     <p className="w-75 m-auto">Help us spread the word about dSocial to your friends and earn 200 RIX for each platform you post on. Click the buttons below to spread the word.</p>
                 </div>
