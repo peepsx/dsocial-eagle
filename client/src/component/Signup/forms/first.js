969efe1b618fec62723ab93f8d4f5fb0e262c6d2
sbsashu <sbsashu143@gmail.com>
2020-07-21 14:36:29 +0530

    user

--- client/src/component/Signup/forms/first.js
+++ client/src/component/Signup/forms/first.js
@@ -204,7 +204,7 @@ export default class First extends React.Component {
                             nextBtnStatus={this.state.nextBtnStatus}
                         />
                     </div>
-                    <div className={!(this.state.nextBtnStatus === 'Twitter') ? 'noClick col-sm mb-3 mb-sm-0 change' : 'col-sm mb-3 mb-sm-0 change'}>
+                    <div className='noClick col-sm mb-3 mb-sm-0 change'>
                         {/* <TwitterLogin
                             authCallback={this.twitterHandler}
                             consumerKey={env.twitter_consumer_key}
