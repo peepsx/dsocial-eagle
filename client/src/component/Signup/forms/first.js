534056b13d060f2071ae606fab209b4be0a477fc
sbsashu <sbsashu143@gmail.com>
2020-07-30 21:59:16 +0530

    disabled div

--- client/src/component/Signup/forms/first.js
+++ client/src/component/Signup/forms/first.js
@@ -211,7 +211,7 @@ export default class First extends React.Component {
                             nextBtnStatus={this.state.nextBtnStatus}
                         />
                     </div>
-                    <div className='col-sm mb-3 mb-sm-0 change'>
+                    <div className='col-sm mb-3 mb-sm-0 change' disabled={localStorage.getItem('twitter_login')}>
                         {/* <TwitterLogin
                             authCallback={this.twitterHandler}
                             consumerKey={env.twitter_consumer_key}
