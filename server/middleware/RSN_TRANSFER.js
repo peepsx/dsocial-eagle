5b24ec9b1c7cedb503639bacc05f8bb204a0631b
sbsashu <sbsashu143@gmail.com>
2020-07-31 18:06:40 +0530

    changes

--- server/middleware/RSN_TRANSFER.js
+++ server/middleware/RSN_TRANSFER.js
@@ -9,50 +9,47 @@ let jwt = require('jsonwebtoken');
 module.exports = {
     RSN_TRANSFER: async (req, res, next) => {
         let { fbUserId, googleEmail, /**instaUserId, teleUserId, */ twitterScreenName } = req.body.userDetails;
-        if( !googleEmail || !twitterScreenName || /**!instaUserId ||  !teleUserId || */ !fbUserId) {
-            return res.status(200).send({
-                success: false,
-                message: 'Fields are missing'
-            })
-        }
+
         if(fbUserId == undefined  || googleEmail == undefined || /**instaUserId == undefined || */ twitterScreenName == undefined) {
             return res.status(200).send({
                 message: 'Fields are missing'
             })
         }
-        let facebook = await TempFacebook.findOne({facebookid: fbUserId});
-        let google = await TempGoogle.findOne({GmailAddress: googleEmail});
-        // let instagram = await TempInstagram.findOne({username: instaUserId});
-        let twitter = await TempTwitter.findOne({username: twitterScreenName});
-        // let telegram = await TempTelegram.findOne({telegram_id: teleUserId});
-        if(!facebook.facebookid || !facebook.fbUserName || facebook === null ) {
-            return res.status(401).send({
-                success: false,
-                message: 'Please Login with Facebook'
-            })
-        } else if(!google.GmailAddress || google === null) {
-            return res.status(401).send({
-                success: false,
-                message: 'Please Login with Google'
-            })
-        } /**else if(!instagram.instaid || !instagram.username || instagram === null) {
-            return res.status(401).send({
-                success: false,
-                message: 'Please Login with Instagram'
-            })
-        } */ else if(!twitter.username || twitter === null) {
-            return res.status(401).send({
-                success: false,
-                message: 'Please Login with Twitter'
-            })
-        }/** else if(!telegram.telegram_id || telegram === null) {
-            return res.status(401).send({
-                success: false,
-                message: 'Please Login with Twitter'
-            })
-        } */
-        else {
+        if(fbUserId) {
+            let facebook = await TempFacebook.findOne({facebookid: fbUserId});
+            if(!facebook.facebookid || !facebook.fbUserName || facebook === null ) {
+                return res.status(401).send({
+                    success: false,
+                    message: 'Please Login with Facebook'
+                })
+            }
+        }
+        if(googleEmail) {
+            let google = await TempGoogle.findOne({GmailAddress: googleEmail});
+            if(!google.GmailAddress || google === null) {
+                return res.status(401).send({
+                    success: false,
+                    message: 'Please Login with Google'
+                })
+            }
+        }
+        if(twitterScreenName) {
+            let twitter = await TempTwitter.findOne({username: twitterScreenName});
+            if(!twitter.username || twitter === null) {
+                return res.status(401).send({
+                    success: false,
+                    message: 'Please Login with Twitter'
+                })
+            }
+        }
+        if(fbUserId || googleEmail || twitterScreenName) {
             next();
+
+        }
+        else {
+            return res.status(200).send({
+                message: 'Fields are missing'
+            })
         }
     },
     Access_Token: async (req, res, next) => {
