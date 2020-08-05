7ae478927de1b40cec9547150ec15f633096c7c0
sbsashu <sbsashu143@gmail.com>
2020-08-05 16:02:48 +0530

    share_reward

--- client/src/component/Signup/forms/third.js
+++ client/src/component/Signup/forms/third.js
@@ -13,7 +13,6 @@ export default class Third extends React.Component {
         this.state = {
             fbPostResponse: '',
             loading: false,
-            amount: 0,
             facebook_share_reward: 0,
             twitter_share_reward: 0
         }
@@ -145,7 +144,7 @@ export default class Third extends React.Component {
                         if (res.data.success) {
                             window.location.hash = "#fifth";
                             localStorage.setItem('s3', true)
-                            localStorage.setItem('share_reward', this.state.amount)
+                            localStorage.setItem('share_reward', this.state.facebook_share_reward + this.state.twitter_share_reward)
                         } else if(!res.data.success) {
                             Swal.fire({
                                 title: 'Error',
@@ -188,7 +187,7 @@ export default class Third extends React.Component {
         return localStorage.getItem('username') || localStorage.getItem('twitterName') || localStorage.getItem('googleEmail') || localStorage.getItem('fbUserId') ? (
             <div className="card-body py-4">
                 <div className="mb-4 text-center">
-                    <img src={gold} alt='gold' width="15 px" height="auto"></img> <span>{parseInt(localStorage.getItem('login_reward')) + parseInt(localStorage.getItem('like_reward') || 0) + parseInt(this.state.amount)} RIX</span>
+                    <img src={gold} alt='gold' width="15 px" height="auto"></img> <span>{parseInt(localStorage.getItem('login_reward')) + parseInt(localStorage.getItem('like_reward') || 0) + parseInt(this.state.facebook_share_reward) + parseInt(this.state.twitter_share_reward)} RIX</span>
                     <span className="h4 d-block">Spread the word about dSocial...</span>
                     <p className="w-75 m-auto">Help us spread the word about dSocial to your friends and earn 200 RIX for each platform you post on. Click the buttons below to spread the word.</p>
                 </div>
