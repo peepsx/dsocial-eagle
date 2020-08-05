cd88ee31ea5ec422bf4240b45ceebb73b8462d7d
sbsashu <sbsashu143@gmail.com>
2020-08-05 22:36:24 +0530

    size of facebook share page

--- client/src/component/Signup/forms/third.js
+++ client/src/component/Signup/forms/third.js
@@ -24,7 +24,7 @@ export default class Third extends React.Component {
             method: 'feed',
             quote: "I just created an account on dSocial, the world's first decentralized and %23censorshipresistant social network. You can join the social revolution and earn 1000 coins in the process. Join us at https://dsocial.network",
             link: 'https://dsocial.network',
-            size: {width:500,height:400}, width:500, height:480
+            size: {width:500, height:400}
         }, (response) => {
             this.setState({ fbPostResponse: response });
             Axios({
